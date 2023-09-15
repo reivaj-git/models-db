8689b8d8787b6e0fdd2a9414488c5d0c9cd253e0
Ricardo <javi26028@outlook.com>
2023-09-14 21:04:24 -0600

    Se modifica database por error de ortografia

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,15 +3,15 @@ import "dotenv/config";
 
 const db = new Sequelize({
   host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME ,
-  database: process.env.DB_NAME ,
-  port: process.env.DB_PORT ,
+  username: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   dialect: "postgres",
-  dialectOptions: {ssl: {require: true, rejectUnauthorized: false}}
+  dialectOptions: {ssl: {required: true, rejectUnauthorized: false}}
 });
 
-// para local comentra dialecteOptions
+// para local comentar dialecteOptions
 
 export default db;
 
